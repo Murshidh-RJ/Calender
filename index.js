e30f49c1099a0e6c00f2b3ff775ca30992ae3e5d
Abdhulla Murshidh <murshidhraji2003@gmail.com>
2024-08-11 16:39:51 +0530

    last commint

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const dayNameEl = document.getElementById("day-name");
 const dayNumEl = document.getElementById("day-number");
 const yearEl = document.getElementById("year");
 
-const date = new Date()
-console.log(date)
+const date = new Date();
+const month = date.getMonth();
+monthNameEl.innerText = date.toLocaleString("en", { month: "long" });
+
+dayNameEl.innerText = date.toLocaleString("en", { weekday: "long" });
+
+dayNumEl.innerText = date.getDate();
+
+yearEl.innerText = date.getFullYear();
